3422236967a21155d41a4db72686234cc346a460
Paul-HenryN <paul-henry.ngankam@2025.ucac-icam.com>
2023-08-06 12:20:19 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import Sidebar from "./Sidebar";
 import "./index.css";
 
 export default function App() {
   return (
-    <div className="w-full h-screen grid grid-cols-[max-content_auto] grid-rows-[5fr_1fr] bg-black">
-      <div className="w-[1000px] bg-green-500">Sidebar</div>
-      <div className="bg-red-500">Main Content</div>
-      <div className="col-span-2 bg-yellow-500">Player</div>
+    <div className="w-full h-screen grid grid-cols-[max-content_auto] grid-rows-[5fr_1fr] gap-y-2 p-2 bg-black">
+      <Sidebar />
+      <div className="bg-neutral-700">Main Content</div>
+      <div className="col-span-2 bg-neutral-700">Player</div>
     </div>
   );
 }
